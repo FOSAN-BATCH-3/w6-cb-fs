78152ad1541819f4a38be07122071a798c165df5
Abdul4212 <57661556+Abdul4212@users.noreply.github.com>
2020-12-09 19:46:44 +0700

    tolong dicek

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
-
+  fs.readFile(parent_file,'utf8',function (err, berhasil) {
+    if (err) {
+      console.log(err)
+    }
+    else{
+      console.log('Loading');
+      sleep(2000)
+      fs.readFile(children_file,'utf8',function (err,sukses) {
+        if (err) {
+          console.log(err);
+        }
+        else{
+          console.log('Loading');
+          sleep(2000)
+          var dataKeluarga = JSON.parse(berhasil)
+          // console.log(dataKeluarga);
+          for (let i = 0; i < dataKeluarga.length; i++) {
+            let dataAnak = JSON.parse(sukses)
+            let tampungAnak = []; 
+            for (let j = 0; j < dataAnak.length; j++) {
+              if (dataKeluarga[i].last_name == dataAnak[j].family) {
+                tampungAnak.push(dataAnak[j].full_name);
+              }
+            }
+            dataKeluarga[i].children = tampungAnak;
+            console.log(dataKeluarga[i]);
+          }
+        }
+      })
+    }
+  })
+  
 }
 
 match_data('./parents.json', './children.json')
