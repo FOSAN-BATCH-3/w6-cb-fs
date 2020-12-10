1abb0dc9c1b3b797feb6ff5d3a5d0a5e9d3f9bc8
Linux <rizki.st3004@gmail.com>
2020-12-10 19:12:11 +0700

    bar

--- index.js
+++ index.js
@@ -11,7 +11,34 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
-
+  fs.readFile(parent_file, 'utf8', function (err, data) {
+    if (err)  {
+      console.log(err)
+    } else  {
+      sleep(1000)
+      console.log('loading data parent.....')
+      sleep(1000)
+      console.log('loading data children.....')
+      sleep(3000)
+      let dataParent = JSON.parse(data)
+        fs.readFile(children_file, 'utf8', function (err, data) {
+          if (err)  {
+            console.log(err)
+          } else  {
+            let dataChildren = JSON.parse(data)
+            for (let i=0; i<dataParent.length; i++) {
+            let penampung = []
+            for (let j=0; j<dataChildren.length; j++) {
+              if (dataParent[i].last_name===dataChildren[j].family)
+                penampung.push(dataChildren[j].full_name)
+                dataParent[i].children = penampung
+            }
+          }
+          console.log(dataParent)
+        }
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
