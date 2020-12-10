ab05e93945313edb5689ac9cb63c1f191495dce6
ramaaziz <rama.aziz77@gmail.com>
2020-12-10 18:40:00 +0700

    akhirnya mudeng

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// const { parse } = require('path');
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -9,9 +10,35 @@ function sleep(milliseconds) {
   }
 }
 
-function match_data(parent_file, children_file) {
-  // Code here
-
+function match_data(parent_file, children_file){ 
+  fs.readFile(parent_file, 'utf8', (err, data) => {
+    console.log('loading data parent....');
+    sleep(2000)
+    if(err){
+      console.log(err);
+    }else{
+      let dataParent = JSON.parse(data);
+      fs.readFile(children_file, 'utf8', (err, data) => {
+        console.log('loading data children....');
+        sleep(2000)
+        if(err){
+          console.log(err);
+        }else{
+          let dataChildren = JSON.parse(data);
+          for(i= 0; i < dataParent.length; i++){
+            let children = [];
+            for(j= 0; j < dataChildren.length; j++){
+              if(dataParent[i].last_name === dataChildren[j].family){
+                children.push(dataChildren[j].full_name)
+              }
+              dataParent[i].children = children
+            }
+          }
+          console.log(dataParent);
+        }
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
